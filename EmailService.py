8aa2aaf9f03cf1ab00f3f3ce800b01aff45acd51
Fox.hu <Hujia0523@gmail.com>
2019-11-02 20:34:56 +0800

    update .gitignore

--- EmailService.py
+++ EmailService.py
@@ -4,19 +4,19 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-import ExcelDemo
+import ExcelService
 import smtplib
 import email
 
 # 邮箱、账户、发件人、收件人、抄送信息
-mailservice = 'smtp.163.com'
+smtpService = 'smtp.163.com'
 userName = 'spring_boot@163.com'
 password = 'abc123455'
 target = 'youjia2062@163.com'
 cc = '308585610@qq.com'
 
 # 获取excel的信息
-content = ExcelDemo.getExcelData(16, 19)
+content = ExcelService.getExcelData(16, 19)
 email = MIMEMultipart()
 
 # todo 构建正文
@@ -34,7 +34,7 @@ att.add_header('Content-Disposition', 'attachment', filename=Header('test.xlsx',
 encoders.encode_base64(att)
 email.attach(att)
 
-smtp = smtplib.SMTP_SSL(mailservice, port=465)
+smtp = smtplib.SMTP_SSL(smtpService, port=465)
 smtp.login(userName, password)
 smtp.sendmail(userName, target, email.as_string())
 
